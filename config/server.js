1c95160897badcfa8970f762d9751886190f7a06
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-04-16 13:53:26 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df2780d9f3346e9d5fa33158ff531291'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d5f227f-8e91-4a69-bbb6-3c85ffb6f328'
+};
